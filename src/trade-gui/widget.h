a5415f52e7b12edfd03aaf280c9539f487a7cc0b
Yibang Heng <yibang.heng@outlook.com>
2024-04-30 17:44:59 +0800

    Add trade-gui

---
+++ src/trade-gui/widget.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+
+class Widget;
+
+}
+QT_END_NAMESPACE
+
+class Widget final: public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget* parent = nullptr);
+    ~Widget() override;
+
+private:
+    Ui::Widget* ui;
+};
