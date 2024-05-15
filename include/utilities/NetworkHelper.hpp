250489ba12ad8d79727aea5e78d24d93c9e341c7
Yibang Heng <yibang.heng@outlook.com>
2024-05-15 16:06:25 +0800

    Add RRServer and RRClient and its tests

--- include/utilities/NetworkHelper.hpp
+++ include/utilities/NetworkHelper.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <fmt/format.h>
 #include <google/protobuf/message.h>
+#include <zmq.h>
 
 #include "networks.pb.h"
 
@@ -77,6 +79,126 @@ private:
 
         return static_cast<int32_t>(uval);
     }
+};
+
+struct ZMQContextPtrDeleter {
+    void operator()(void* ptr) const
+    {
+        zmq_ctx_destroy(ptr);
+    }
+};
+
+/// TODO: Use std::shared_ptr and factory pattern here.
+using ZMQContextPtr = std::unique_ptr<void, ZMQContextPtrDeleter>;
+
+struct ZMQSocketPtrDeleter {
+    void operator()(void* ptr) const
+    {
+        zmq_close(ptr);
+    }
+};
+
+using ZMQSocketPtr = std::unique_ptr<void, ZMQSocketPtrDeleter>;
+
+class RRServer
+{
+public:
+    explicit RRServer(const std::string& address, void* context = nullptr) : zmq_request()
+    {
+        zmq_context = context != nullptr ? context : zmq_ctx_new();
+        zmq_socket.reset(::zmq_socket(zmq_context, ZMQ_REP));
+
+        const auto code = zmq_bind(zmq_socket.get(), address.c_str());
+
+        if (code != 0) {
+            throw std::runtime_error(fmt::format("Failed to bind ZMQ socket at {}: {}", address, std::string(zmq_strerror(errno))));
+        }
+    }
+    ~RRServer()
+    {
+        zmq_msg_close(&zmq_request);
+    }
+
+public:
+    [[nodiscard]] auto receive()
+    {
+        zmq_msg_init(&zmq_request);
+
+        const auto code = zmq_msg_recv(&zmq_request, zmq_socket.get(), 0);
+
+        if (code < 0)
+            return std::make_tuple(types::MessageID::invalid_message_id, std::string {});
+
+        return Serializer::deserialize(std::string(static_cast<char*>(zmq_msg_data(&zmq_request)), zmq_msg_size(&zmq_request)));
+    }
+
+    void send(const types::MessageID message_id, const google::protobuf::Message& message_body) const
+    {
+        const auto message = Serializer::serialize(message_id, message_body);
+
+        zmq_send(zmq_socket.get(), message.c_str(), message.size(), ZMQ_DONTWAIT);
+    }
+
+private:
+    void* zmq_context;
+    ZMQSocketPtr zmq_socket;
+    zmq_msg_t zmq_request;
+};
+
+class RRClient
+{
+public:
+    explicit RRClient(const std::string& address, void* context = nullptr) : zmq_response()
+    {
+        zmq_context = context != nullptr ? context : zmq_ctx_new();
+        zmq_socket.reset(::zmq_socket(zmq_context, ZMQ_REQ));
+
+        const auto code = zmq_connect(zmq_socket.get(), address.c_str());
+
+        if (code != 0) {
+            throw std::runtime_error(fmt::format("Failed to connect ZMQ socket to {}: {}", address, std::string(zmq_strerror(errno))));
+        }
+    }
+    ~RRClient()
+    {
+        zmq_msg_close(&zmq_response);
+    }
+
+public:
+    template<typename ReturnedMessageType>
+    ReturnedMessageType request(
+        const types::MessageID message_id,
+        const google::protobuf::Message& message
+    )
+    {
+        /// Send request.
+        const auto message_body = Serializer::serialize(message_id, message);
+
+        zmq_send(zmq_socket.get(), message_body.c_str(), message_body.size(), 0);
+
+        /// Receive response.
+        zmq_msg_init(&zmq_response);
+
+        zmq_msg_recv(&zmq_response, zmq_socket.get(), 0);
+
+        const auto [response_id, response_body] = Serializer::deserialize({static_cast<char*>(zmq_msg_data(&zmq_response)), zmq_msg_size(&zmq_response)});
+
+        zmq_msg_close(&zmq_response);
+
+        /// Return an empty message if the response is invalid.
+        if (response_id == types::MessageID::invalid_message_id)
+            return ReturnedMessageType {};
+
+        ReturnedMessageType returned_message;
+        returned_message.ParseFromString(response_body);
+
+        return returned_message;
+    }
+
+private:
+    void* zmq_context;
+    ZMQSocketPtr zmq_socket;
+    zmq_msg_t zmq_response;
 };
 
 } // namespace trade::utilities
--- tests/NetworkHelperTest.cpp
+++ tests/NetworkHelperTest.cpp
@@ -1,6 +1,10 @@
 #include <catch.hpp>
+#include <thread>
 
 #include "utilities/NetworkHelper.hpp"
+
+constexpr size_t insertion_times = 16;
+constexpr size_t insertion_batch = 1024;
 
 TEST_CASE("Message with message_id == 0 and empty message", "[Serializer]")
 {
@@ -85,4 +89,52 @@ TEST_CASE("Message with an out-of-range message_id", "[Serializer]")
 
     CHECK(message_id == trade::types::MessageID::invalid_message_id);
     CHECK(unix_sig.sig() == 2); /// The returned message still contains the original value.
+}
+
+TEST_CASE("Sending and receiving messages with M:1 connections", "[RRServer/RRClient]")
+{
+    trade::utilities::ZMQContextPtr zmq_context;
+    zmq_context.reset(zmq_ctx_new());
+
+    /// Server side.
+    auto server_worker = [&zmq_context] {
+        trade::utilities::RRServer server("inproc://server", zmq_context.get());
+
+        for (int i = 0; i < insertion_times * insertion_batch; i++) {
+            const auto [message_id, message_body] = server.receive();
+
+            trade::types::UnixSig unix_sig;
+            unix_sig.ParseFromString(message_body);
+
+            CHECK(message_id == trade::types::MessageID::unix_sig);
+
+            server.send(trade::types::MessageID::unix_sig, unix_sig);
+        }
+    };
+
+    std::thread server_thread(server_worker);
+
+    /// Client side.
+    auto client_worker = [&zmq_context] {
+        trade::utilities::RRClient client("inproc://server", zmq_context.get());
+
+        for (int i = 0; i < insertion_batch; i++) {
+            trade::types::UnixSig send_unix_sig;
+            send_unix_sig.set_sig(i);
+
+            const auto received_unix_sig = client.request<trade::types::UnixSig>(trade::types::MessageID::unix_sig, send_unix_sig);
+
+            CHECK(received_unix_sig.sig() == i);
+        }
+    };
+
+    std::array<std::thread, insertion_times> client_threads;
+
+    for (auto& client_thread : client_threads)
+        client_thread = std::thread(client_worker);
+
+    for (auto& client_thread : client_threads)
+        client_thread.join();
+
+    server_thread.join();
 }
