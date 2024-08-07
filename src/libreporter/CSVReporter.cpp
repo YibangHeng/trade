37c6b6bce45ece58d72ae784a1e8821dcda0c9ca
Yibang Heng <yibang.heng@outlook.com>
2024-08-07 17:00:54 +0800

    Add ranged ticks support in CSVReporter

--- src/libreporter/CSVReporter.cpp
+++ src/libreporter/CSVReporter.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <ranges>
 
 #include "libreporter/CSVReporter.h"
 #include "utilities/TimeHelper.hpp"
@@ -6,8 +7,10 @@
 trade::reporter::CSVReporter::~CSVReporter()
 {
     /// Flush all writers.
-    for (auto& [symbol, writer] : m_l2_tick_writers)
-        writer.flush();
+    for (auto& writer : m_l2_tick_writers | std::views::values)
+        writer.close();
+    for (auto& writer : m_ranged_tick_writers | std::views::values)
+        writer.close();
 }
 
 void trade::reporter::CSVReporter::l2_tick_generated(const std::shared_ptr<types::GeneratedL2Tick> generated_l2_tick)
@@ -49,6 +52,61 @@ void trade::reporter::CSVReporter::l2_tick_generated(const std::shared_ptr<types
     m_outside->l2_tick_generated(generated_l2_tick);
 }
 
+void trade::reporter::CSVReporter::ranged_tick_generated(const std::shared_ptr<types::RangedTick> ranged_tick)
+{
+    new_ranged_tick_writer(ranged_tick->symbol());
+
+    m_ranged_tick_writers[ranged_tick->symbol()] << fmt::format(
+        "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}",
+        ranged_tick->symbol(),
+        ranged_tick->start_time(),
+        ranged_tick->end_time(),
+        ranged_tick->exchange_time(),
+        ranged_tick->ask_levels().at(4).price_1000x(),
+        ranged_tick->ask_levels().at(3).price_1000x(),
+        ranged_tick->ask_levels().at(2).price_1000x(),
+        ranged_tick->ask_levels().at(1).price_1000x(),
+        ranged_tick->ask_levels().at(0).price_1000x(),
+        ranged_tick->bid_levels().at(0).price_1000x(),
+        ranged_tick->bid_levels().at(1).price_1000x(),
+        ranged_tick->bid_levels().at(2).price_1000x(),
+        ranged_tick->bid_levels().at(3).price_1000x(),
+        ranged_tick->bid_levels().at(4).price_1000x(),
+        ranged_tick->active_traded_sell_number(),
+        ranged_tick->active_sell_number(),
+        ranged_tick->active_sell_quantity(),
+        ranged_tick->active_sell_amount_1000x(),
+        ranged_tick->active_traded_buy_number(),
+        ranged_tick->active_buy_number(),
+        ranged_tick->active_buy_quantity(),
+        ranged_tick->active_buy_amount_1000x(),
+        ranged_tick->weighted_ask_price().at(4),
+        ranged_tick->weighted_ask_price().at(3),
+        ranged_tick->weighted_ask_price().at(2),
+        ranged_tick->weighted_ask_price().at(1),
+        ranged_tick->weighted_ask_price().at(0),
+        ranged_tick->weighted_bid_price().at(0),
+        ranged_tick->weighted_bid_price().at(1),
+        ranged_tick->weighted_bid_price().at(2),
+        ranged_tick->weighted_bid_price().at(3),
+        ranged_tick->weighted_bid_price().at(4),
+        ranged_tick->aggressive_sell_number(),
+        ranged_tick->aggressive_buy_number(),
+        ranged_tick->new_added_ask_1_quantity(),
+        ranged_tick->new_added_bid_1_quantity(),
+        ranged_tick->new_canceled_ask_1_quantity(),
+        ranged_tick->new_canceled_bid_1_quantity(),
+        ranged_tick->new_canceled_ask_all_quantity(),
+        ranged_tick->new_canceled_bid_all_quantity(),
+        ranged_tick->big_ask_amount_1000x(),
+        ranged_tick->big_bid_amount_1000x(),
+        ranged_tick->highest_price_1000x(),
+        ranged_tick->lowest_price_1000x(),
+        ranged_tick->ask_price_1_valid_duration_1000x(),
+        ranged_tick->bid_price_1_valid_duration_1000x()
+    );
+}
+
 void trade::reporter::CSVReporter::new_l2_tick_writer(const std::string& symbol)
 {
     if (!m_l2_tick_writers.contains(symbol)) [[unlikely]] {
@@ -88,6 +146,67 @@ void trade::reporter::CSVReporter::new_l2_tick_writer(const std::string& symbol)
             /// Time.
             << "exchange_time,"
             << "local_system_time"
+            << std::endl;
+    }
+}
+
+void trade::reporter::CSVReporter::new_ranged_tick_writer(const std::string& symbol)
+{
+    if (!m_ranged_tick_writers.contains(symbol)) [[unlikely]] {
+        const std::filesystem::path file_path = fmt::format("{}/{}/{}-ranged-tick.csv", m_output_folder, utilities::Date<std::string>()(), symbol);
+
+        create_directories(std::filesystem::path(file_path).parent_path());
+        m_ranged_tick_writers.emplace(symbol, std::ofstream(file_path));
+
+        logger->info("Opened new ranged tick writer at {}", file_path.string());
+
+        m_ranged_tick_writers[symbol]
+            << "symbol,"
+            << "start_time,"
+            << "end_time,"
+            << "exchange_time,"
+            << "sell_price_1000x_5,"
+            << "sell_price_1000x_4,"
+            << "sell_price_1000x_3,"
+            << "sell_price_1000x_2,"
+            << "sell_price_1000x_1,"
+            << "buy_price_1000x_1,"
+            << "buy_price_1000x_2,"
+            << "buy_price_1000x_3,"
+            << "buy_price_1000x_4,"
+            << "buy_price_1000x_5,"
+            << "active_traded_sell_number,"
+            << "active_sell_number,"
+            << "active_sell_quantity,"
+            << "active_sell_amount_1000x,"
+            << "active_traded_buy_number,"
+            << "active_buy_number,"
+            << "active_buy_quantity,"
+            << "active_buy_amount_1000x,"
+            << "weighted_ask_price_5,"
+            << "weighted_ask_price_4,"
+            << "weighted_ask_price_3,"
+            << "weighted_ask_price_2,"
+            << "weighted_ask_price_1,"
+            << "weighted_bid_price_1,"
+            << "weighted_bid_price_2,"
+            << "weighted_bid_price_3,"
+            << "weighted_bid_price_4,"
+            << "weighted_bid_price_5,"
+            << "aggressive_sell_number,"
+            << "aggressive_buy_number,"
+            << "new_added_ask_1_quantity,"
+            << "new_added_bid_1_quantity,"
+            << "new_canceled_ask_1_quantity,"
+            << "new_canceled_bid_1_quantity,"
+            << "new_canceled_ask_all_quantity,"
+            << "new_canceled_bid_all_quantity,"
+            << "big_ask_amount_1000x,"
+            << "big_bid_amount_1000x,"
+            << "highest_price_1000x,"
+            << "lowest_price_1000x,"
+            << "ask_price_1_valid_duration_1000x,"
+            << "bid_price_1_valid_duration_1000x"
             << std::endl;
     }
 }
